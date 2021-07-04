f84bf9dd719f1259fe472d948bdc1d85f761efff
naeun <kne040@daum.net>
2021-07-05 02:30:19 +0900

    21.07.05 02:28am 나은
    coupon action,db패키지 생성
    couponDAO 생성
    (멤버컨트롤러 수정사항 있음)
    주문 상세조희 페이지 수정
    css 수정

--- src/com/coupon/action/PointcheckAction.java
+++ src/com/coupon/action/PointcheckAction.java
@@ -1,16 +1,10 @@
-package com.order.action;
-
-import java.util.List;
-import java.util.Vector;
+package com.coupon.action;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import com.order.db.OrderDAO;
-import com.order.db.OrderDTO;
-
-public class getOrderListAction implements Action{
+public class PointcheckAction implements Action{
 
 	@Override
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
@@ -24,18 +18,11 @@ public class getOrderListAction implements Action{
 			forward.setRedirect(true);
 			return forward;
 		}
-		OrderDAO odao = new OrderDAO();
-		Vector totalList = odao.getOrderList(userId);
-		request.setAttribute("orderList", totalList.get(0));
-		request.setAttribute("goodsList", totalList.get(1));
 		
 		
 		
 		
-		forward.setPath("./goods_order/showorder.jsp");
-		forward.setRedirect(false);
-		return forward;
+		return null;
 	}
-
 	
 }
