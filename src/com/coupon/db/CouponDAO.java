91104548e9d83e140938601f9cfd10d4cac579b3
fenne <fenne@DESKTOP-OMCUJ3E>
2021-07-08 19:53:04 +0900

    21.07.08 couponDAO 수정

--- src/com/coupon/db/CouponDAO.java
+++ src/com/coupon/db/CouponDAO.java
@@ -5,6 +5,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -108,6 +109,163 @@ public class CouponDAO {
 	}
 	// mycouponlist(userId);
 	
+	//insertCoupon(userId)
+    public void insertCoupon(String userId){
+       
+       int num=0;
+       
+       try {
+       conn = getConnection();
+
+       sql = "select max(mcNum) from my_coupon";
+    
+          pstmt = conn.prepareStatement(sql);
+          
+          rs = pstmt.executeQuery();
+          
+          System.out.println("회원번호: "+num);
+       
+       if(rs.next()){
+          num = rs.getInt(1)+1;
+       }
+          
+       
+       sql = "insert into my_coupon values(?,1,?,1)";
+       
+          pstmt.setInt(1, num);
+          pstmt.setString(2, userId);
+          
+          pstmt.executeUpdate(); //insert, update, delete => int 형이라서 rs로 받을수 없음.
+       
+       } catch (SQLException e) {            
+          e.printStackTrace();   
+       }finally{
+          closeDB();
+       }
+
+    }//
+
+    //insertCoupon(userId)
+	
+   
+    //pointcheck(startdate,enddate)
+    public List pointcheck(String startdate,String enddate,String userId){
+    	System.out.println(startdate);
+    	System.out.println(enddate);
+    	
+    	List orderList = new ArrayList();
+    	try {
+    		conn = getConnection();
+    		sql = "select distinct o_tradeNum,orderDate,addPoint,cpUseAmount,ptUseAmount,o_cosName from order_board where orderDate between ? and ? and o_userId = ?";
+    		pstmt = conn.prepareStatement(sql);
+    		pstmt.setString(1, startdate);
+    		pstmt.setString(2, enddate);
+    		pstmt.setString(3, userId);
+    		
+    		rs =pstmt.executeQuery();
+    		System.out.println("sql 실행완료....");
+    		if(!rs.next()){
+    			System.out.println("없음 test중");
+    			}else{
+    			
+    			while(rs.next()){
+    				OrderDTO odto = new OrderDTO();
+    				odto.setOrderDate(rs.getDate("orderDate"));
+    				odto.setPtUseAmount(rs.getInt("ptUseAmount"));
+    				odto.setAddPoint(rs.getInt("addPoint"));
+    				odto.setO_tradeNum(rs.getString("o_tradeNum"));
+    				odto.setO_cosName(rs.getString("o_cosName"));
+    				
+    				orderList.add(odto);
+    				System.out.println(odto);
+    			
+    				}
+    			}
+    	}catch (SQLException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}finally {
+    		closeDB();
+    	}
+    	System.out.println(orderList);
+    	return orderList;
+    }
+    //pointcheck(startdate,enddate)
+    
+    //pointcheck2(searchmonth1,userId);
+    public List pointcheck2(String searchmonth2,String userId){
+    	
+    	List orderList2 = new ArrayList();
+    	try {
+    		conn = getConnection();
+    		sql = "select distinct o_tradeNum,orderDate,addPoint,cpUseAmount,ptUseAmount,o_cosName from order_board where orderDate between ? and now() and o_userId = ?";
+    		pstmt = conn.prepareStatement(sql);
+    		pstmt.setString(1, searchmonth2);
+    		pstmt.setString(2, userId);
+    		
+    		rs =pstmt.executeQuery();
+    		System.out.println("sql 실행완료....제발유...");
+    		if(!rs.next()){
+    			System.out.println("없음 test중");
+    			}else{
+    			
+    			while(rs.next()){
+    				OrderDTO odto2 = new OrderDTO();
+    				odto2.setOrderDate(rs.getDate("orderDate"));
+    				odto2.setPtUseAmount(rs.getInt("ptUseAmount"));
+    				odto2.setAddPoint(rs.getInt("addPoint"));
+    				odto2.setO_tradeNum(rs.getString("o_tradeNum"));
+    				odto2.setO_cosName(rs.getString("o_cosName"));
+    				
+    				orderList2.add(odto2);
+    				System.out.println(odto2);
+    			
+    				}
+    			}
+    	}catch (SQLException e) {
+    		// TODO Auto-generated catch block
+    		e.printStackTrace();
+    	}finally {
+    		closeDB();
+    	}
+    	System.out.println(orderList2);
+    	return orderList2;
+    }
+    
+    //pointcheck2(searchmonth1,userId);
+    
+	//couponNumList(): 쿠폰 번호 배열에 저장
+	public List couponNumList(){
+		
+		List couponNumList = new ArrayList();
+		
+		try {
+			conn=getConnection();
+			sql="select couponNum from coupon_type";
+			//1:가입축하 10% (회원가입시 자동 발급), 2:생일 20% 할인쿠폰, 3:전회원 10% 할인, 4: 여름맞이 50% 할인, 5: 첫구매 30%
+			pstmt=conn.prepareStatement(sql);
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()){
+				CouponDTO cdto = new CouponDTO();
+				
+				cdto.setCouponNum(rs.getInt("couponNum"));
+				
+				couponNumList.add(cdto);
+			}
+			
+				System.out.println("쿠폰번호 리스트에 저장완료");
+				System.out.println("쿠폰번호 리스트: "+couponNumList);
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		} finally {
+			closeDB();
+		}
+		
+		return couponNumList;
+		
+	}//couponNumList()
 	
 	//insertCoupon(userId, mcCouponNum): 발급된 쿠폰번호를 my_coupon에 insert
     public void insertCoupon(String userId, int mcCouponNum){
@@ -135,6 +293,7 @@ public class CouponDAO {
           
        		pstmt.executeUpdate(); //insert, update, delete => int 형이라서 rs로 받을수 없음.
        
+       		System.out.println("쿠폰 다운 완료: "+num+","+mcCouponNum+","+userId);
        } catch (SQLException e) {            
           e.printStackTrace();   
        }finally{
@@ -143,39 +302,8 @@ public class CouponDAO {
 
     }
     //insertCoupon(userId, mcCouponNum)
-    
-	//couponNumList(): 쿠폰 번호 배열에 저장
-	public List couponNumList(){
-		
-		List couponNumList = new ArrayList();
-		
-		try {
-			conn=getConnection();
-			sql="select couponNum from coupon_type";
-			//1:가입축하 10% (회원가입시 자동 발급), 2:생일 20% 할인쿠폰, 여기부터 사용-> 3:전회원 10% 할인, 4: 여름맞이 50% 할인, 5: 첫구매 30%
-			pstmt=conn.prepareStatement(sql);
-			rs = pstmt.executeQuery();
-			
-			while(rs.next()){
 
-				CouponDTO cdto = new CouponDTO();
-				
-				cdto.setCouponNum(rs.getInt("couponNum"));
-				
-				couponNumList.add(cdto);
-			}
-			
-				System.out.println("쿠폰번호 리스트에 저장완료");
-				System.out.println("쿠폰번호 리스트: "+couponNumList);
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-		} finally {
-			closeDB();
-		}
-		
-		return couponNumList;
-		
-	}//couponNumList()
-   
+    
+    
+
 }
